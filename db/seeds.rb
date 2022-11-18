2c7e0ac71d3bce780781565d1facaad1e1964060
Antoine Mariani <antoine.mariani@me.com>
2022-11-18 16:46:51 +0100

    last push with smoother front & decent movie seed through API

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,21 +9,42 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 require 'faker'
+require 'open-uri'
+require 'json'
 
-Movie.create(title: 'Wonder Woman 1984',
-             overview: 'Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s', poster_url: 'https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg', rating: 6.9)
-Movie.create(title: 'The Shawshank Redemption',
-             overview: 'Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison', poster_url: 'https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg', rating: 8.7)
-Movie.create(title: 'Titanic',
-             overview: '101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.', poster_url: 'https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg', rating: 7.9)
-Movie.create(title: "Ocean's Eight",
-             overview: 'Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.', poster_url: 'https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg', rating: 7.0)
+page = 1
 
-30.times do
-  Movie.create(
-    title: Faker::Movie.title,
-    overview: Faker::Movie.quote,
-    poster_url: 'https://picsum.photos/200/300',
-    rating: rand(0.0..5.0).round(1)
-  )
+3.times do
+  puts "Starting to seed page #{page}"
+  n = 0
+  20.times do
+    puts "Starting to seed result at index #{n}"
+    url = "https://api.themoviedb.org/3/movie/top_rated?api_key=2a3967e1d44e8206d68d2d365366f7bd&language=en-US&page=#{page}"
+    json = JSON.parse(URI.open(url).read)
+    Movie.create(
+      title: json.values[1][n].values[5],
+      overview: json.values[1][n].values[6],
+      poster_url: "https://image.tmdb.org/t/p/w500#{json.values[1][n].values[8]}",
+      rating: json.values[1][n].values[12]
+    )
+    puts "Finished seeding result at index #{n}"
+    n += 1
+  end
+  puts "Finished seeding page #{page}"
+  page += 1
 end
+puts 'Seeding complete'
+
+# movie = Movie.create(
+#   title: Faker::Movie.title,
+#   overview: Faker::Movie.quote,
+#   poster_url: 'https://picsum.photos/200/300',
+#   rating: rand(0.0..10.0).round(1)
+# )
+
+  # url = "https://api.themoviedb.org/3/search/movie?api_key=2a3967e1d44e8206d68d2d365366f7bd&query=#{movie.title.split(' ').join('%20')}&page=1&include_adult=false"
+  # json = JSON.parse(URI.open(url).read)
+  # movie.poster_url = "https://image.tmdb.org/t/p/w500#{json.values[1][0].values[8]}"
+  # movie.overview = json.values[1][0].values[8]
+  # movie.rating = json.values[1][0].values[12]
+# url = "https://api.themoviedb.org/3/search/movie?api_key=2a3967e1d44e8206d68d2d365366f7bd&query=#{'fight club'.split(' ').join('%20')}&page=1&include_adult=false"
